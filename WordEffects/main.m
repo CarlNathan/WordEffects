5cf1452d7325ecb3ba1b4322f72b45349baaab37
CarlNathan <c.n.udren@gmail.com>
2016-01-11 15:38:44 -0500

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Carl Udren on 1/11/16.
+//  Copyright © 2016 Carl Udren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
