e7aba21baa876bde6ad38a9a8f789b13be8e74c5
CarlNathan <c.n.udren@gmail.com>
2016-01-11 19:02:59 -0500

    Finish the project

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -9,9 +9,82 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    {
+        BOOL infiniteLoopCondition = YES;
+        NSArray *options = @[@"1. Uppercase", @"2. Lowercase", @"3.Numberize", @"4.Canadianize", @"5.Respond", @"6.De-space-it"];
+       while (infiniteLoopCondition) {
+        
+            char inputChars[255];
+            printf("input a string: ");
+            scanf("%s", inputChars);
+            printf("Your string is %s\n", inputChars);
+
+           NSLog(@"%@", options);
+           
+            int inputOperation;
+            printf("Enter your desired operation(1-6):");
+            scanf("%d", &inputOperation);
+            printf("Your number is %d\n", inputOperation);
+
+        
+        
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            NSLog(@"Input was: %@", inputString);
+            NSString *response = [[NSString alloc] init];
+            NSInteger asInt;
+           
+            switch (inputOperation) {
+                case 1:
+                    response = [inputString uppercaseString];
+                    NSLog(@"%@",response);
+                    break;
+                    
+                case 2:
+                    response = [inputString lowercaseString];
+                    NSLog(@"%@",response);
+                    break;
+                    
+                case 3:
+                    asInt = [inputString integerValue];
+                    if (asInt == 0) {
+                        if ([inputString isEqualToString:@"0"]) {
+                            NSLog(@"Conversion Completed: %ld",(long)asInt);
+                            break;
+                        }else{
+                            NSLog(@"Conversion could not be completed");
+                            break;
+                        }
+                    }
+                    NSLog(@"Conversion completed: %ld",(long)asInt);
+                    break;
+                    
+                case 4:
+                    response = [inputString stringByAppendingString:@", eh?"];
+                    NSLog(@"%@",response);
+                    break;
+                    
+                case 5:
+                    if ([inputString hasSuffix:@"?"]) {
+                        NSLog(@"I don't know");
+                    } else if ([inputString hasSuffix:@"!"]){
+                        NSLog(@"Whoa, calm down!");
+
+                    }else {
+                        NSLog(@"Sounds good");
+                    }
+                    break;
+                    
+                case 6:
+                    response = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+                    NSLog(@"%@", response);
+                    break;
+                    
+                default:
+                    NSLog(@"You have not choosen a valid operation.  Please try again.");
+                    break;
+            }
+        }
+        
+   }
     return 0;
 }
